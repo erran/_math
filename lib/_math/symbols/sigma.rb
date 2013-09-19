b4741ffa43668a55d598da475ffd5976db90f830
Erran Carey <me@errancarey.com>
2013-09-19 00:09:27 -0500

    Update _math/symbols/{sigma,sqrt}.rb

--- lib/_math/symbols/sigma.rb
+++ lib/_math/symbols/sigma.rb
@@ -2,24 +2,22 @@
 
 # A module for use in summation
 module Sigma
-  class << self
-    # Returns the summartion of the addends
-    #
-    # @note this method will yield the iterator if a length 3 containing the
-    #   lower bound of summation, upper bound of summation, and a Proc that
-    #   accepts the iterator is given
-    # @param [Numeric] addends a list of numbers to find the sum of
-    # @return [Numeric,Sigma] if arguments were supplied a numeric value is
-    #   returned, otherwise Sigma is returned
-    def [](*addends)
-      if addends.count.eql?(3) && addends.last.kind_of?(Proc)
-        addends = (addends[0]..addends[1]).to_a.map do |i|
-          addends.last.call(i.to_f)
-        end
+  # Returns the summartion of the addends
+  #
+  # @note this method will yield the iterator if a length 3 containing the
+  #   lower bound of summation, upper bound of summation, and a Proc that
+  #   accepts the iterator is given
+  # @param [Numeric] addends a list of numbers to find the sum of
+  # @return [Numeric,Sigma] if arguments were supplied a numeric value is
+  #   returned, otherwise Sigma is returned
+  def self.[](*addends)
+    if addends.count.eql?(3) && addends.last.kind_of?(Proc)
+      addends = (addends[0]..addends[1]).to_a.map do |i|
+        addends.last.call(i.to_f)
       end
-
-      addends.map(&:to_f).reduce(:+)
     end
+
+    addends.map(&:to_f).reduce(:+)
   end
 end
 
--- lib/_math/symbols/sqrt.rb
+++ lib/_math/symbols/sqrt.rb
@@ -1,8 +1,15 @@
 # encoding: utf-8
 
-# Returns the square root of a number
-#
-# @param [Numeric] num the number to take the square root of
-def √(num)
-  Math.sqrt(num)
+module Sqrt
+  # Returns the square root of a number
+  #
+  # @param [Numeric] num the number to take the square root of
+  def self.[](num)
+    Math.sqrt(num)
+  end
+end
+
+# @see Sqrt[]
+def √(num = false)
+  num ? Sqrt[num] : Sqrt
 end
