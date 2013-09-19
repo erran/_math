4ee7659f5c32e6ce34747efd9263d89d0bc3e8e7
Erran Carey <me@errancarey.com>
2013-09-19 02:13:54 -0500

    Update lib/_math/symbols/sigma.rb

--- lib/_math/symbols/sigma.rb
+++ lib/_math/symbols/sigma.rb
@@ -2,28 +2,33 @@
 
 # A module for use in summation
 module Sigma
-  # Returns the summartion of the addends
-  #
-  # @note this method will yield the iterator if a length 3 containing the
-  #   lower bound of summation, upper bound of summation, and a Proc that
-  #   accepts the iterator is given
-  # @param [Numeric] addends a list of numbers to find the sum of
-  # @return [Numeric,Sigma] if arguments were supplied a numeric value is
-  #   returned, otherwise Sigma is returned
-  def self.[](*addends)
-    addends.flatten!
+  class << self
+    # Returns the summartion of the addends
+    #
+    # @note this method will yield the iterator if a length 3 containing the
+    #   lower bound of summation, upper bound of summation, and a Proc that
+    #   accepts the iterator is given
+    # @param [Numeric] addends a list of numbers to find the sum of
+    # @return [Numeric,Sigma] if arguments were supplied a numeric value is
+    #   returned, otherwise Sigma is returned
+    def summation(*addends)
+      addends.flatten!
 
-    if addends.count.eql?(3) && addends.last.kind_of?(Proc)
-      addends = (addends[0]..addends[1]).to_a.map do |i|
-        addends.last.call(i.to_f)
+      if addends.last.kind_of?(Proc)
+        range = addends.count == 2 ? addends.first : addends.first..addends[1]
+
+        addends = range.map do |i|
+          addends.last.call(i.to_f)
+        end
       end
-    end
 
-    addends.map(&:to_f).reduce(:+)
+      addends.map(&:to_f).reduce(:+)
+    end
+    alias_method :[], :summation
   end
 end
 
-# Returns the result of Sigma.[] or Sigma depending on arguments being supplied
+# Returns the result of Sigma.summation or Sigma depending on arguments being supplied
 #
 # @note this method will yield the iterator if a length 3 containing the
 #   lower bound of summation, upper bound of summation, and a Proc that
